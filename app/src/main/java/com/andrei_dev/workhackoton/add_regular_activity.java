c1f77b7ac38be3509054c2fa0c4d4bb7130fe763
andrei <andrei.n15@mail.ru>
2017-12-04 13:41:27 +0600

    modified aminActivity

--- app/src/main/java/com/andrei_dev/workhackoton/add_regular_activity.java
+++ app/src/main/java/com/andrei_dev/workhackoton/add_regular_activity.java
@@ -1,5 +1,6 @@
 package com.andrei_dev.workhackoton;
 
+import android.content.ClipboardManager;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -7,28 +8,67 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.TextView;
 
 import com.andrei_dev.SQLite.DBHelper;
 
 
 public class add_regular_activity extends AppCompatActivity {
     DBHelper dbHelper;
-    String[] cities = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"};
+    String[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"};
+
+    EditText subjects;
+    Byte[] pairs = {1, 2, 3, 4, 5, 6, 7, 8};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_regular_activity);
-         dbHelper = new DBHelper(this);
-        Spinner spinner = (Spinner) findViewById(R.id.Day);
-        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cities);
-        // Определяем разметку для использования при выборе элемента
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Применяем адаптер к элементу spinner
-        spinner.setAdapter(adapter);
-    }}
+
+        dbHelper = new DBHelper(this);
+        selection = (TextView) findViewById(R.id.selection);
+
+        Spinner Days = (Spinner) findViewById(R.id.Day);
+
+        Spinner Pair = (Spinner) findViewById(R.id.Pair);
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, days);
+
+        ArrayAdapter<Byte> adapter3 = new ArrayAdapter<Byte>(this, android.R.layout.simple_spinner_item, pairs);
+
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Days.setAdapter(adapter1);
+
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Pair.setAdapter(adapter3);
+    }
+    OnItemSelectedListener itemSelectedListener = new OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            // Получаем выбранный объект
+            String item = (String)parent.getItemAtPosition(position);
+            selection.setText(item);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+        spinner.setOnItemSelectedListener(itemSelectedListener);
+}
+
+}
+
+
 
 //    public void onClick(View v) {
 //
